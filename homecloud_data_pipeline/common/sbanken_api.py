a662264c64355cf6afa0f15a8f7ca604f23b96d0
Ole Martin Ruud <dev@barskern.no>
2021-11-14 02:55:52 +0100

    Initial commit

---
+++ homecloud_data_pipeline/common/sbanken_api.py
@@ -0,0 +1,192 @@
+from typing import Any, Dict, List, Optional
+from datetime import datetime, date
+import json
+import logging
+import os
+from urllib.parse import quote
+
+from authlib.integrations.requests_client.oauth2_session import OAuth2Session
+
+logging.basicConfig(level="DEBUG")
+logger = logging.getLogger(__name__)
+
+API_URL = "https://publicapi.sbanken.no/apibeta"
+CACHE_FOLDER = "cache"
+
+
+def get_oauth_session(client_id: str, client_secret: str):
+    token_filename = f"{CACHE_FOLDER}/token.json"
+    token = None
+    try:
+        with open(token_filename) as file:
+            token = json.load(file)
+            logger.info(f"Found token in file '{token_filename}'")
+
+            since_expiration = datetime.now().timestamp() - token["expires_at"]
+            if since_expiration > 0:
+                logger.info(f"Token expired {since_expiration:.0f} seconds ago")
+                token = None
+            else:
+                logger.info(
+                    f"Token will expire in {-since_expiration:.0f} seconds, so continue using it"
+                )
+    except FileNotFoundError:
+        pass
+
+    oauth = OAuth2Session(
+        client_id=quote(client_id),
+        client_secret=quote(client_secret),
+        token=token,
+    )
+
+    if token is None:
+        logger.info("Token was expired or missing, generating a new token...")
+
+        token = oauth.fetch_token(
+            url="https://auth.sbanken.no/identityserver/connect/token",
+        )
+
+        with open(token_filename, "w") as file:
+            json.dump(token, file)
+        logger.info(f"Dumped new token to '{token_filename}'")
+
+    return oauth
+
+
+def query_or_cache(
+    oauth: OAuth2Session, name: str, url: str, params: Optional[Dict[str, str]] = None
+):
+    filepath = f"{CACHE_FOLDER}/{name}.json"
+    try:
+        with open(filepath) as file:
+            data = json.load(file)
+        logger.debug(f"Fetched data for '{name}' from cache at '{filepath}'")
+    except FileNotFoundError:
+        result = oauth.get(url, params=params)
+
+        if result.status_code == 200:
+            data = result.json()
+
+            os.makedirs(CACHE_FOLDER, exist_ok=True)
+            with open(filepath, "w") as file:
+                json.dump(data, file)
+            logger.debug(
+                f"Fetched data for '{name}' from API and cached it to '{filepath}'"
+            )
+        else:
+            logger.error(result.status_code)
+            logger.error(result.text)
+            exit(1)
+
+    return data
+
+
+def fetch_transactions_for_account(
+    oauth: OAuth2Session, account_id: str, start_date: date, end_date: date
+):
+    if start_date == end_date:
+        name = f"{start_date}-{account_id}-transactions"
+    else:
+        name = f"{start_date}-{end_date}-{account_id}-transactions"
+
+    transactions = query_or_cache(
+        oauth,
+        name,
+        f"{API_URL}/api/v2/Transactions/archive/{account_id}",
+        {"startDate": start_date.isoformat(), "endDate": end_date.isoformat()},
+    )
+    logger.info(
+        f"Found {transactions['availableItems']} transactions for {account_id} from {start_date} to {end_date}"
+    )
+    return transactions
+
+
+def fetch_accounts(oauth: OAuth2Session, execution_date: date):
+    name = f"{execution_date.isoformat()}-accounts"
+
+    accounts = query_or_cache(
+        oauth,
+        name,
+        f"{API_URL}/api/v2/Accounts",
+    )
+    logger.info(f"Found {accounts['availableItems']} accounts the {execution_date}")
+    return accounts
+
+def fetch_cards(oauth: OAuth2Session, execution_date: date):
+    name = f"{execution_date.isoformat()}-cards"
+
+    cards = query_or_cache(
+        oauth,
+        name,
+        f"{API_URL}/api/v2/Cards",
+    )
+    logger.info(f"Found {cards['availableItems']} cards the {execution_date}")
+    return cards
+
+def fetch_customer(oauth: OAuth2Session, execution_date: date):
+    name = f"{execution_date.isoformat()}-customer"
+
+    customer = query_or_cache(
+        oauth,
+        name,
+        f"{API_URL}/api/v2/Customers",
+    )
+    logger.info(f"Found a customer with id {customer['customerId']} the {execution_date}")
+    return customer
+
+
+def store_raw_data(
+    data: Any, name: str, data_folder: str, subfolders: List[str] = None
+) -> str:
+    """Store the data to the given folder with the naming given"""
+    subfolders_ = "/".join(subfolders) + "/" if isinstance(subfolders, list) else ""
+
+    filepath = f"{data_folder}/{subfolders_}{name}.json"
+    os.makedirs(f"{data_folder}/{subfolders_}", exist_ok=True)
+
+    with open(filepath, "w") as file:
+        json.dump(data, file)
+    logger.info(f"Stored '{name}' data to '{filepath}'")
+
+    return filepath
+
+
+# start_date = date(2021, 11, 1)
+# execution_date = date.today()
+# oauth = get_oauth_session(CLIENT_ID, CLIENT_SECRET)
+# 
+# execution_date_s = execution_date.isoformat()
+# 
+# accounts = query_or_cache(oauth, "accounts", f"{API_URL}/api/v2/Accounts")
+# store_raw_data(accounts, f"accounts-{execution_date_s}", RAW_DATA_FOLDER)
+# logger.info(f"Found {accounts['availableItems']} accounts")
+# 
+# cards = query_or_cache(oauth, "cards", f"{API_URL}/api/v2/Cards")
+# store_raw_data(cards, f"cards-{execution_date_s}", RAW_DATA_FOLDER)
+# logger.info(f"Found {cards['availableItems']} cards")
+# 
+# customer = query_or_cache(oauth, "customer", f"{API_URL}/api/v2/Customers")
+# store_raw_data(customer, f"customer-{execution_date_s}", RAW_DATA_FOLDER)
+# logger.info(f"Found customer {customer['customerId']}")
+# 
+# query_date = start_date
+# for _ in range(3):
+#     for account in accounts["items"]:
+#         account_id = account["accountId"]
+# 
+#         transactions = fetch_transactions_for_account(
+#             oauth, account_id, query_date, query_date
+#         )
+#         store_raw_data(
+#             transactions,
+#             account_id,
+#             RAW_DATA_FOLDER,
+#             subfolders=[
+#                 "transactions",
+#                 f"{query_date.year:04d}",
+#                 f"{query_date.month:02d}",
+#                 f"{query_date.day:02d}",
+#             ],
+#         )
+# 
+#     query_date = query_date + timedelta(days=1)
---
+++ homecloud_data_pipeline/dags/sbanken_dag.py
@@ -0,0 +1,117 @@
+import logging
+import json
+from pprint import pp
+from datetime import datetime, timedelta, date
+
+from airflow.decorators import dag, task
+from airflow.operators.python import get_current_context
+from airflow.models import Variable
+
+# from homecloud_data_pipeline.common.workday import AfterWorkdayTimetable
+# from airflow.example_dags.plugins.workday import AfterWorkdayTimetable
+
+logger = logging.getLogger(__name__)
+
+
+@dag(
+    start_date=datetime(2020, 1, 1),
+    schedule_interval="@daily",
+    # timetable=AfterWorkdayTimetable(),
+)
+def sbanken_etl():
+    @task(multiple_outputs=True)
+    def extract_metadata():
+        """
+        Fetch data which is not time dependent, but we store them in a
+        'time-dependent' way to be able to see changes etc.
+        """
+        from homecloud_data_pipeline.common.sbanken_api import (
+            store_raw_data,
+            get_oauth_session,
+            fetch_accounts,
+            fetch_cards,
+            fetch_customer,
+        )
+
+        raw_datafolder = "data/0-raw/sbanken_etl"
+
+        client_id = Variable.get("SBANKEN_CLIENT_ID")
+        client_secret = Variable.get("SBANKEN_CLIENT_SECRET")
+        oauth = get_oauth_session(client_id, client_secret)
+
+        context = get_current_context()
+        execution_date = context["execution_date"]
+
+        accounts = fetch_accounts(oauth, execution_date)
+        accounts_path = store_raw_data(
+            accounts, f"accounts-{execution_date.to_date_string()}", raw_datafolder
+        )
+        cards = fetch_cards(oauth, execution_date)
+        cards_path = store_raw_data(
+            cards, f"cards-{execution_date.to_date_string()}", raw_datafolder
+        )
+        customer = fetch_customer(oauth, execution_date)
+        customer_path = store_raw_data(
+            customer, f"customer-{execution_date.to_date_string()}", raw_datafolder
+        )
+        return {
+            "account_ids": [account["accountId"] for account in accounts["items"]],
+            "accounts_path": accounts_path,
+            "cards_path": cards_path,
+            "customer_path": customer_path,
+        }
+
+    @task()
+    def extract_transactions(account_ids):
+        """Fetch archived transactions for the given day"""
+        from homecloud_data_pipeline.common.sbanken_api import (
+            store_raw_data,
+            get_oauth_session,
+            fetch_transactions_for_account,
+        )
+        raw_datafolder = "data/0-raw/sbanken_etl"
+
+        context = get_current_context()
+        start_date = context["data_interval_start"]
+        end_date = context["data_interval_end"]
+        if start_date + timedelta(days=1) != end_date:
+            raise RuntimeError("DAG only configured for daily workloads!")
+
+        query_date = start_date
+
+        client_id = Variable.get("SBANKEN_CLIENT_ID")
+        client_secret = Variable.get("SBANKEN_CLIENT_SECRET")
+        oauth = get_oauth_session(client_id, client_secret)
+
+        for account_id in account_ids:
+            transactions = fetch_transactions_for_account(
+                oauth,
+                account_id,
+                query_date,
+                query_date,
+            )
+            store_raw_data(
+                transactions,
+                account_id,
+                raw_datafolder,
+                subfolders=[
+                    "transactions",
+                    f"{query_date.year:04d}",
+                    f"{query_date.month:02d}",
+                    f"{query_date.day:02d}",
+                ],
+            )
+
+    @task()
+    def transform():
+        pass
+
+    @task()
+    def load():
+        pass
+
+    metadata = extract_metadata()
+    extract_transactions(metadata['account_ids'])
+
+
+sbanken_dag = sbanken_etl()
